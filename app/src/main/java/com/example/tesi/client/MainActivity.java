e1464081ece27ca1f02023e9cb5d628711be25df
andreasasso9 <andry-sasso@outlook.it>
2024-05-03 23:02:35 +0200

    aggiunta pagina signup e aggiornata login, aggiunta entity user

--- app/src/main/java/com/example/tesi/client/MainActivity.java
+++ app/src/main/java/com/example/tesi/client/MainActivity.java
@@ -1,15 +1,20 @@
 package com.example.tesi.client;
 
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+	private boolean loggedIn;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		loggedIn=false;
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.login_layout);
+		if (loggedIn)
+			setContentView(R.layout.activity_main);
+		else
+			setContentView(R.layout.login_layout);
+
 
 
 	}
---
+++ app/src/main/java/com/example/tesi/entity/User.java
@@ -0,0 +1,49 @@
+package com.example.tesi.entity;
+
+import java.util.UUID;
+
+public class User {
+	private String email;
+	private String username;
+	private String password;
+	private UUID id;
+
+	public User(String email, String username, String password, UUID id) {
+		this.email = email;
+		this.username = username;
+		this.password = password;
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+}
