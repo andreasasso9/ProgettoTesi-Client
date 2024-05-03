969e29ad5f3db2c74ce9694b64ead02682e64b84
andreasasso9 <andry-sasso@outlook.it>
2024-05-03 21:49:21 +0200

    Pagina di login creata

---
+++ app/src/main/java/com/example/tesi/client/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.tesi.client;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.login_layout);
+
+
+	}
+}
